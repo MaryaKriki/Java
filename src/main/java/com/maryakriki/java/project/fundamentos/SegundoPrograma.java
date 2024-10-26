968c1941efba77cf310ee6f2fc0cfd543a2eb5c9
MaryaKriki <mariae.kriki@gmail.com>
2024-10-26 16:45:07 -0300

    created second class

---
+++ src/main/java/com/maryakriki/java/project/fundamentos/SegundoPrograma.java
@@ -0,0 +1,8 @@
+package com.maryakriki.java.project.fundamentos;
+
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+        System.out.println();
+    }
+}
