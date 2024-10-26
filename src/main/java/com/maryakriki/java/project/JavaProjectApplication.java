ecbf5629d775e5d74697750e0bc06fcf46ef9028
Marya Kriki <mariae.kriki@gmail.com>
2024-10-26 15:57:49 -0300

    initial commit

---
+++ src/main/java/com/maryakriki/java/project/JavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maryakriki.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+		System.out.println("Hello world.");
+	}
+}
