f3124bdf0561d79fbed6ad8b3a68c006c3b57966
MaryaKriki <mariae.kriki@gmail.com>
2024-10-31 14:32:55 -0300

    comment exercise

---
+++ src/main/java/com/maryakriki/java/project/fundamentos/Comentarios.java
@@ -0,0 +1,24 @@
+package com.maryakriki.java.project.fundamentos;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+        System.out.println();
+        /**
+         * Comentário JAVADOC: é usado para
+         * deixar as informações do projeto
+         *
+         * @author Marya Kriki
+         * @since JAVA21.0.4
+         * @see https://github.com/MaryaKriki
+         */
+
+        //Comentário de uma linha
+
+        /*
+
+        Comentário de mais de uma linha
+
+         */
+    }
+}
--- src/main/java/com/maryakriki/java/project/fundamentos/SegundoPrograma.java
+++
@@ -1,8 +0,0 @@
-package com.maryakriki.java.project.fundamentos;
-
-public class SegundoPrograma {
-    public static void main(String[] args) {
-        System.out.println("xxx");
-        System.out.println();
-    }
-}
