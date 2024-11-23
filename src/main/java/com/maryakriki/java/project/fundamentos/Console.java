10e8b62615076fdefa9fc69d641a9869c5099561
MaryaKriki <mariae.kriki@gmail.com>
2024-11-23 12:07:34 -0300

    console class

--- src/main/java/com/maryakriki/java/project/fundamentos/Console.java
+++ src/main/java/com/maryakriki/java/project/fundamentos/Console.java
@@ -13,11 +13,22 @@ public class Console {
         System.out.printf("Megasena: %d %d %d %d %d %d %n",
                 0, 9, 1, 0, 9, 7);
         System.out.printf("Prêmio: R$%.2f%n", 145000.99);
-        System.out.printf("Ganhador: %s %s %n", "João", "Kikri");
+        System.out.printf("Ganhador: %s %s %n", "João", "Silva");
 
         Scanner entrada = new Scanner(System.in);
 
-        entrada.nextLine();
+        System.out.print("Digite o seu nome: ");
+        String nome = entrada.nextLine();
 
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = entrada.nextLine();
+
+        System.out.print("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+
+        System.out.printf("%s %s tem %d anos.%n",
+                nome, sobrenome, idade);
+
+        entrada.close();
     }
 }
