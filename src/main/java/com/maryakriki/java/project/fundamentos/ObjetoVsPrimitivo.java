6370e0ed54f7227f28252edd742418044407474a
MaryaKriki <mariae.kriki@gmail.com>
2024-11-23 12:48:40 -0300

    Objects vs Primitive Types

--- src/main/java/com/maryakriki/java/project/fundamentos/ObjetoVsPrimitivo.java
+++ src/main/java/com/maryakriki/java/project/fundamentos/ObjetoVsPrimitivo.java
@@ -1,4 +1,17 @@
 package com.maryakriki.java.project.fundamentos;
 
 public class ObjetoVsPrimitivo {
+    public static void main(String[] args) {
+
+        // String é um classe assim como ObjetoVsPrimitivo, o objeto é o valor atribuido á classe
+        //ex.
+        String s = new String("texto");
+        //String = classe, "texto" = valor/objeto
+
+        s.toUpperCase();
+        // as classes (ex. String) tem métodos dentro dele, ex. toUpperCase
+
+        int a = 123;
+        // tipos primitivos não têm classes, então os seus "objetos" são na verdade Wrappers
+    }
 }
