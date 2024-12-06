0cf27135adff21e519166619783472751b614feb
MaryaKriki <mariae.kriki@gmail.com>
2024-12-06 13:10:13 -0300

    exercising conversion

---
+++ src/main/java/com/maryakriki/java/project/fundamentos/DesafioConversao.java
@@ -0,0 +1,30 @@
+package com.maryakriki.java.project.fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioConversao {
+    public static void main(String[] args) {
+
+        Scanner salario = new Scanner(System.in);
+
+        System.out.println("Digite seu salario do primeiro mês: ");
+        String salario1 = salario.nextLine().replace(",",".");
+
+        System.out.println("Digite seu salario do segundo mês: ");
+        String salario2 = salario.nextLine().replace(",",".");
+
+        System.out.println("Digite seu salario do terceiro mês: ");
+        String salario3 = salario.nextLine().replace(",",".");
+
+
+        double val1 = Double.parseDouble(salario1);
+        double val2 = Double.parseDouble(salario2);
+        double val3 = Double.parseDouble(salario3);
+
+        double soma = val1 + val2 + val3;
+        System.out.println("O total desse trimestre foi: " + soma);
+        System.out.println("Sua média por mês é: " + soma / 3);
+
+        salario.close();
+    }
+}
