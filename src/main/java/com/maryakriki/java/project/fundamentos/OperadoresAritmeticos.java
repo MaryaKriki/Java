10b6dee213e3b86cc2b1db1547b7b93bb81af6b2
MaryaKriki <mariae.kriki@gmail.com>
2024-12-10 13:12:17 -0300

    arithmetic operators

---
+++ src/main/java/com/maryakriki/java/project/fundamentos/OperadoresAritmeticos.java
@@ -0,0 +1,29 @@
+package com.maryakriki.java.project.fundamentos;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        System.out.println(2 + 3);
+
+        var x = 7.84;
+        double y = 2.27;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int a = 7;
+        int b = 4;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a / (double) b);
+
+        System.out.println(10 % 3);
+
+        System.out.println(a + b - x * y);
+    }
+}
