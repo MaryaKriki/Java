38a3d583374299032434868b167f4af1d6b97811
MaryaKriki <mariae.kriki@gmail.com>
2024-12-17 14:03:48 -0300

    ternary operators

---
+++ src/main/java/com/maryakriki/java/project/fundamentos/operadores/OperadoresTernarios.java
@@ -0,0 +1,13 @@
+package com.maryakriki.java.project.fundamentos.operadores;
+
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+
+        double media = 7.6;
+        String resultado = media >= 7.0 ? "aprovado." : "em recuperação.";
+
+
+        System.out.println("O aluno está " + resultado);
+    }
+}
--- src/main/java/com/maryakriki/java/project/fundamentos/operadores/ParOuImpar.java
+++ src/main/java/com/maryakriki/java/project/fundamentos/operadores/ParOuImpar.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 public class ParOuImpar {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
         System.out.println("Escreva um número: ");
         int numero = entrada.nextInt();
 
         numero %= 2;
 
-        entrada.close();
 
         if (numero == 0) {
             System.out.println("O número é par");
-        } else {
+        }
+        else {
             System.out.println("O número é ímpar");
         }
+
+        entrada.close();
     }
 }
