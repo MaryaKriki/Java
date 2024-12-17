82f70ac1355bf893922d4ddc7e049b6f8969f732
MaryaKriki <mariae.kriki@gmail.com>
2024-12-17 13:02:47 -0300

    operators

--- src/main/java/com/maryakriki/java/project/fundamentos/operadores/OperadoresUnarios.java
+++ src/main/java/com/maryakriki/java/project/fundamentos/operadores/OperadoresUnarios.java
@@ -6,20 +6,24 @@ public class OperadoresUnarios {
         int a = 1;
         int b = 2;
 
-        a++; //a = a + 1
-        a--; //a = a - 1
+         a++; //a = a + 1
+         a--; //a = a - 1
 
-        ++b; //b = b + 1
-        --b; //b = b - 1
+         ++b; //b = b + 1
+         --b; //b = b - 1
+         System.out.println(a);
+         System.out.println(b);
 
-        System.out.println(a);
-        System.out.println(b);
+         System.out.println("Mini desafio...");
+         System.out.println(++a == b--);
+         System.out.println(a == b);
 
-        System.out.println("Mini desafio...");
-        System.out.println(++a == b--);
-        System.out.println(a == b);
+         System.out.println(a);
+         System.out.println(b);
+//         var lista = 3;
+//        for(int i = 0; i < lista.length; i++) {
+//            System.out.println( lista[i] );
+//        }
 
-        System.out.println(a);
-        System.out.println(b);
     }
 }
--- src/main/java/com/maryakriki/java/project/fundamentos/operadores/ParOuImpar.java
+++ src/main/java/com/maryakriki/java/project/fundamentos/operadores/ParOuImpar.java
@@ -1,4 +1,21 @@
 package com.maryakriki.java.project.fundamentos.operadores;
 
+import java.util.Scanner;
+
 public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Escreva um número: ");
+        int numero = entrada.nextInt();
+
+        numero %= 2;
+
+        entrada.close();
+
+        if (numero == 0) {
+            System.out.println("O número é par");
+        } else {
+            System.out.println("O número é ímpar");
+        }
+    }
 }
