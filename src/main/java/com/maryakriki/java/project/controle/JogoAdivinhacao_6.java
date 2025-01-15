b6581ca9d9a76b87361f7f6004c674b2e1891aef
MaryaKriki <mariae.kriki@gmail.com>
2025-01-15 16:30:49 -0300

    module exercise #06

--- src/main/java/com/maryakriki/java/project/controle/JogoAdivinhacao_6.java
+++ src/main/java/com/maryakriki/java/project/controle/JogoAdivinhacao_6.java
@@ -1,12 +1,34 @@
 package com.maryakriki.java.project.controle;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class JogoAdivinhacao_6 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        Random random = new Random();
 
+        int num = random.nextInt(100);
+        
+        System.out.println("Adivinhe um número de 0 a 100: ");
+        int tentativa = entrada.nextInt();
 
+        for (int contador = 10; contador > 0; contador--){
+                if (tentativa < num && tentativa != num){
+                    System.out.println("Você errou!! Tem mais " + contador + " tentativas.");
+                    System.out.println("DICA: O número gerado é maior que o digitado.");
+                    tentativa = entrada.nextInt();
+                } else if (tentativa > num && tentativa != num){
+                    System.out.println("Você errou!! Tem mais " + contador + " tentativas.");
+                    System.out.println("DICA: O número gerado é menor que o digitado.");
+                    tentativa = entrada.nextInt();
+                }
+
+        }
+
+        if (tentativa == num){
+            System.out.println("Parabéns você adivinhou!!!");
+        }
 
         entrada.close();
     }
