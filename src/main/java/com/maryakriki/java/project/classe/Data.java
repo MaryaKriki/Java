67f5b78fb1ee2ea9916f9e4b6dc1aab7e3f98079
MaryaKriki <mariae.kriki@gmail.com>
2025-01-29 13:14:33 -0300

    constructor exercise

--- src/main/java/com/maryakriki/java/project/classe/Data.java
+++ src/main/java/com/maryakriki/java/project/classe/Data.java
@@ -5,6 +5,19 @@ public class Data {
     String mes;
     int ano;
 
+    Data(){
+        dia = 1;
+        mes = "Janeiro";
+        ano = 1970;
+
+    }
+
+    public Data(int dia, String mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
     String obterData(){
         return String.format("Data de hoje: " + this.dia + this.mes + this.ano);
     }
--- src/main/java/com/maryakriki/java/project/classe/DataTeste.java
+++ src/main/java/com/maryakriki/java/project/classe/DataTeste.java
@@ -3,14 +3,8 @@ package com.maryakriki.java.project.classe;
 public class DataTeste {
     public static void main(String[] args) {
         Data d1 = new Data();
-        d1.dia = 23;
-        d1.mes = " / Janeiro / ";
-        d1.ano = 2025;
 
-        Data d2 = new Data();
-        d2.dia = 12;
-        d2.mes = " / Julho / ";
-        d2.ano = 2025;
+        Data d2 = new Data(31, "Dezembro", 1969);
 
         System.out.println(d1.obterData());
         System.out.println("Data do meu aniversário de 15 anos: " + d2.dia + d2.mes + d2.ano);
