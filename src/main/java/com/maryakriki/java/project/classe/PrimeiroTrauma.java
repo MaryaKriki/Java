b52bfc1e47e57218b973bf990d40ad92ea5992a7
MaryaKriki <mariae.kriki@gmail.com>
2025-02-08 16:12:34 -0300

    first trauma exercise answer

--- src/main/java/com/maryakriki/java/project/classe/PrimeiroTrauma.java
+++ src/main/java/com/maryakriki/java/project/classe/PrimeiroTrauma.java
@@ -2,8 +2,10 @@ package com.maryakriki.java.project.classe;
 
 public class PrimeiroTrauma {
     int a = 3;
+    static int b = 4;
     public static void main(String[] args) {
         PrimeiroTrauma pt = new PrimeiroTrauma();
         System.out.println(pt.a);
+        System.out.println(b);
     }
 }
